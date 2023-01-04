f78599a388b2b7f5cb4ca4677d83e4de4b26f8de
David <117180189+Agnonimoose@users.noreply.github.com>
2023-01-04 05:25:44 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='bulk_crawler',
+    packages=['bulk_crawler'],
+    include_package_data=True,
+    install_requires=[
+        'aiohttp',
+    ],
+)
